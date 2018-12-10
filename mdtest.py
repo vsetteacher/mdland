af90a116364f4bf34c450ce3d1004097c91937ea
vsetteacher <weining.peng@gmail.com>
2018-12-10 15:47:31 +0800

    Update mdtest.py

--- mdtest.py
+++ mdtest.py
@@ -2,6 +2,8 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from random import random
 # define testing class
 class mdTest(unittest.TestCase):
     def setUp(self):
@@ -52,8 +54,69 @@ class mdTest(unittest.TestCase):
         self.driver.switch_to.frame(iframe)
         elem = self.driver.find_element_by_id("txtPatientLastName")
         elem.clear()
+
         elem.send_keys("Peng")
+        elem = self.driver.find_element_by_id("txtPatientFirstName")
+        elem.clear()
+        elem.send_keys("Richard")
+        elem = self.driver.find_element_by_id("txtPatientDOB")
+        elem.clear()
+        elem.send_keys("12/07/1997")
+        elem = self.driver.find_element_by_xpath("//*[@name='rdoPatientGender' and @value='M']")
+        elem.click()
+        elemList = self.driver.find_elements_by_xpath('//div[@id="divReg"]/table//input')
+        for result in elemList:
+            try:
+                result.send_keys("133666666")
+            except:
+                continue
+        elemList = self.driver.find_elements_by_xpath('//div[@id="divReg"]/table//select')
+
+        for result in elemList:
+                select = Select(result)
+                select.select_by_index(2)
+
+        elemList = self.driver.find_elements_by_link_text("Edit")
+        elemList[0].click()
+
+        elem = self.driver.find_element_by_xpath("//*[@name='PatientMutiRace']")
+        elem.click()
+
+        elem = self.driver.find_element_by_link_text("Save")
+        elem.click()
+
+        elemList[1].click()
+
+        elem = self.driver.find_element_by_xpath("//*[@name='PatientMutiEthnicity']")
+        elem.click()
+
+        elem = self.driver.find_element_by_link_text("Save")
+        elem.click()
+
+        elem = self.driver.find_element_by_xpath("//div[@id='btnContinue']/table//span[@class='buttonTitle']")
+        elem.click()
+
+        elem = self.driver.find_element_by_link_text("Select")
+        elem.click()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         #self.driver.quit()
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+"""if __name__ == '__main__':
+    unittest.main(verbosity=2)"""
+xTest = mdTest()
+xTest.setUp()
+xTest.testRegistry()
+
